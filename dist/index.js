1f253cb0f2a54126b6b4d6948656289e416061dd
Victor-sarti <Sartivictor24@gmail.com>
2025-10-27 15:28:08 -0300

    enum

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,16 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hi");
-//# sourceMappingURL=index.js.map
+//ENUM 
+//const admin = 1
+//const read = 2
+//const backup = 3
+var Trabalho;
+(function (Trabalho) {
+    Trabalho[Trabalho["admin"] = 1] = "admin";
+    Trabalho[Trabalho["dev"] = 2] = "dev";
+    Trabalho[Trabalho["dash"] = 3] = "dash";
+})(Trabalho || (Trabalho = {}));
+var user = {
+    nome: 'Victor',
+    idade: 20,
+    trabalho: Trabalho.dev
+};
+console.log(user);
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-//TUPLES
+//ENUM 
 
-// Apple = produto, 3 = valor, true = se tem no estoque
-let item: [string, number, boolean] = ['Apple', 3, true]
+//const admin = 1
+//const read = 2
+//const backup = 3
+
+enum Trabalho{admin =1, desenvolvedor =2, powerBi=3}
+
+const user = {
+    nome: 'Victor',
+    idade: 20,
+    trabalho: Trabalho.desenvolvedor
+}
+
+console.log(user)
